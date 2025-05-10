bb54aa7fa35399d6bdb50bd669bf80316a5847e6
白影White <86943191+bywhite0@users.noreply.github.com>
2025-05-10 21:52:26 +0800

    fix: 修复获取专辑封面异常导致崩溃的问题 #3 #9

--- MediaIsland/Helpers/ThumbnailHelper.cs
+++ MediaIsland/Helpers/ThumbnailHelper.cs
@@ -1,53 +1,84 @@
-﻿// From https://github.com/DubyaDude/WindowsMediaController/blob/master/Sample.UI/MainWindow.xaml.cs#L174-L215
+﻿// Modified from https://github.com/DubyaDude/WindowsMediaController/blob/master/Sample.UI/MainWindow.xaml.cs#L174-L215
 // Copyright (c) 2020 DubyaDude
 // Licensed under the MIT License.
-
 using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using Windows.Storage.Streams;
+using Microsoft.Extensions.Logging;
 
 namespace MediaIsland.Helpers
 {
     internal static class ThumbnailHelper
     {
-        internal static BitmapImage? GetThumbnail(IRandomAccessStreamReference Thumbnail, bool convertToPng = true)
+        static ILogger<Plugin>? logger;
+        internal static async Task<ImageSource?> GetThumbnail(IRandomAccessStreamReference? thumbnail, bool convertToPng = true)
         {
-            if (Thumbnail == null)
+            if (thumbnail == null)
                 return null;
 
-            var thumbnailStream = Thumbnail.OpenReadAsync().GetAwaiter().GetResult();
-            byte[] thumbnailBytes = new byte[thumbnailStream.Size];
-            using (DataReader reader = new DataReader(thumbnailStream))
+            try
             {
-                reader.LoadAsync((uint)thumbnailStream.Size).GetAwaiter().GetResult();
-                reader.ReadBytes(thumbnailBytes);
-            }
+                using var thumbnailStream = await thumbnail.OpenReadAsync();
+                if (thumbnailStream.Size == 0)
+                    return null;
 
-            byte[] imageBytes = thumbnailBytes;
-
-            if (convertToPng)
-            {
-                using var fileMemoryStream = new System.IO.MemoryStream(thumbnailBytes);
-                Bitmap thumbnailBitmap = (Bitmap)Bitmap.FromStream(fileMemoryStream);
-
-                if (!thumbnailBitmap.RawFormat.Equals(System.Drawing.Imaging.ImageFormat.Png))
+                byte[] thumbnailBytes = new byte[thumbnailStream.Size];
+                using (var reader = new DataReader(thumbnailStream))
                 {
-                    using var pngMemoryStream = new System.IO.MemoryStream();
-                    thumbnailBitmap.Save(pngMemoryStream, System.Drawing.Imaging.ImageFormat.Png);
-                    imageBytes = pngMemoryStream.ToArray();
+                    await reader.LoadAsync((uint)thumbnailStream.Size);
+                    reader.ReadBytes(thumbnailBytes);
                 }
-            }
 
-            var image = new BitmapImage();
-            using (var ms = new System.IO.MemoryStream(imageBytes))
+                byte[] imageBytes = thumbnailBytes;
+
+                if (convertToPng)
+                {
+                    try
+                    {
+                        using var fileMemoryStream = new MemoryStream(thumbnailBytes);
+                        using var thumbnailBitmap = (Bitmap)Bitmap.FromStream(fileMemoryStream);
+
+                        if (!thumbnailBitmap.RawFormat.Equals(System.Drawing.Imaging.ImageFormat.Png))
+                        {
+                            using var pngMemoryStream = new MemoryStream();
+                            thumbnailBitmap.Save(pngMemoryStream, System.Drawing.Imaging.ImageFormat.Png);
+                            imageBytes = pngMemoryStream.ToArray();
+                        }
+                    }
+                    catch (Exception ex) when (ex is ArgumentException or IOException)
+                    {
+                        logger?.LogError($"将封面转换为 PNG 失败: {ex.Message}");
+                    }
+                }
+
+                var image = new BitmapImage();
+                using (var ms = new MemoryStream(imageBytes))
+                {
+                    image.BeginInit();
+                    image.CacheOption = BitmapCacheOption.OnLoad;
+                    image.StreamSource = ms;
+                    try
+                    {
+                        image.EndInit();
+                    }
+                    catch (NotSupportedException ex)
+                    {
+                        logger?.LogError($"加载图片失败: {ex.Message}");
+                        return null;
+                    }
+                }
+
+                image.Freeze();
+                return image;
+            }
+            catch (Exception ex) when (ex is IOException or COMException)
             {
-                image.BeginInit();
-                image.CacheOption = BitmapCacheOption.OnLoad;
-                image.StreamSource = ms;
-                image.EndInit();
+                logger?.LogError($"处理封面时发生错误: {ex.Message}");
+                return null;
             }
-
-            return image;
         }
     }
 }
