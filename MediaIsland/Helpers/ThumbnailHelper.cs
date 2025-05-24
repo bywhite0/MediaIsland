8d559f4223f7c12eeb5464804df0b3943b1de244
白影White <86943191+bywhite0@users.noreply.github.com>
2025-05-25 02:10:52 +0800

    feat: 移除 Spotify 专辑封面水印

--- MediaIsland/Helpers/ThumbnailHelper.cs
+++ MediaIsland/Helpers/ThumbnailHelper.cs
@@ -8,13 +8,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using Windows.Storage.Streams;
 using Microsoft.Extensions.Logging;
+using System.Windows;
 
 namespace MediaIsland.Helpers
 {
     internal static class ThumbnailHelper
     {
         static ILogger<Plugin>? logger;
-        internal static async Task<ImageSource?> GetThumbnail(IRandomAccessStreamReference? thumbnail, bool convertToPng = true)
+        internal static async Task<ImageSource?> GetThumbnail(IRandomAccessStreamReference? thumbnail, bool convertToPng = true, bool isSourceAppSpotify = false)
         {
             if (thumbnail == null)
                 return null;
@@ -70,6 +71,38 @@ namespace MediaIsland.Helpers
                         return null;
                     }
                 }
+                // 裁剪 Spotify 封面
+                if (isSourceAppSpotify)
+                {
+                    CroppedBitmap croppedBitmap = new(image, new Int32Rect(33, 0, 234, 234));
+                    // 创建一个新的BitmapImage对象
+                    var croppedImage = new BitmapImage();
+
+                    // 将CroppedBitmap保存到MemoryStream
+                    PngBitmapEncoder encoder = new();
+                    encoder.Frames.Add(BitmapFrame.Create(croppedBitmap));
+                    using (var croppedMs = new MemoryStream())
+                    {
+                        encoder.Save(croppedMs);
+                        croppedMs.Position = 0; // 重置流位置到开头
+                        croppedImage.BeginInit();
+                        croppedImage.CacheOption = BitmapCacheOption.OnLoad;
+                        croppedImage.StreamSource = croppedMs;
+                        try
+                        {
+                            croppedImage.EndInit();
+                        }
+                        catch (NotSupportedException ex)
+                        {
+                            logger?.LogError($"加载图片失败: {ex.Message}");
+                            return null;
+                        }
+                    }
+
+                    // 使用裁剪后的图片
+                    image = croppedImage;
+                }
+
 
                 image.Freeze();
                 return image;
