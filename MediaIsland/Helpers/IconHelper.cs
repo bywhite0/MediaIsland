208e891f9e207aec584d37e863fe3042f9861a21
白影White <86943191+bywhite0@users.noreply.github.com>
2025-05-25 09:59:45 +0800

    fix: 修复切换/关闭会话时获取图标报错的问题

--- MediaIsland/Helpers/IconHelper.cs
+++ MediaIsland/Helpers/IconHelper.cs
@@ -119,6 +119,20 @@ namespace MediaIsland.Helpers
                     return IconToImageSourceConverter(GetIconFromPath(AppIDPath, SHGFI_LARGEICON));
                 }
 
+                // 尝试获取内部资源
+                try
+                {
+                    var internalIcon = GetInternalIcon(userModelId);
+                    if (internalIcon != null)
+                    {
+                        return internalIcon;
+                    }
+                }
+                catch
+                {
+                    // 忽略内部资源获取失败
+                }
+
                 // 最终回退方案
                 return IconToImageSourceConverter(SystemIcons.Application);
             }
