25aedeed761836c7ebd184aa16f75212a16df241
白影White <86943191+bywhite0@users.noreply.github.com>
2025-05-31 07:53:30 +0800

    feat: 接入 Sentry

--- MediaIsland/Plugin.cs
+++ MediaIsland/Plugin.cs
@@ -26,6 +26,18 @@ namespace MediaIsland
                 ConfigureFileHelper.SaveConfig<PluginSettings>(Path.Combine(PluginConfigFolder, "Settings.json"), Settings);
             };
             services.AddSettingsPage<IntegrationSettingsPage>();
+#if !DEBUG
+            SentrySdk.Init(o =>
+            {
+                o.Dsn = "https://b86194d67d9ae75813f08deff24ce4f2@o4503936977666048.ingest.us.sentry.io/4509413290606592";
+                o.TracesSampleRate = 1.0;
+                o.Release = Info.Manifest.Version;
+            });
+            ClassIsland.Core.AppBase.Current.DispatcherUnhandledException += (_,e) => {
+                if (e.Exception.StackTrace == null) SentrySdk.CaptureException(e.Exception);
+                else if (e.Exception.StackTrace.Contains("MediaIsland")) SentrySdk.CaptureException(e.Exception);
+            };
+#endif
             Console.WriteLine("[MI]MediaIsland 加载成功");
         }
 
